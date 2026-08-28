49687885728ad54c2f577e04ac67c8ca7c735dd3
Sonakshi Shukla <ssonakshi754@gmail.com>
2026-08-28 12:21:11 +0530

    Time: 4 ms (100%), Space: 46.2 MB (95.24%) - LeetHub

--- 3734-lexicographically-smallest-palindromic-permutation-greater-than-target/3734-lexicographically-smallest-palindromic-permutation-greater-than-target.java
+++ 3734-lexicographically-smallest-palindromic-permutation-greater-than-target/3734-lexicographically-smallest-palindromic-permutation-greater-than-target.java
@@ -1,105 +1,142 @@
 class Solution {
-    public String isPossible(int n, int[] freqIn, String cur, char mid, String target){
-        int[] freq = freqIn.clone(); // copy, since C++ passes freq by value here
 
-        // build the largest possible arrangement of remaining chars (descending order)
-        for(int i=25; i>=0; i--){
-            while(freq[i] > 0){
-                cur += (char)('a'+i);
-                freq[i]--;
+    private String buildPalindrome(String half, char middle) {
+        StringBuilder result = new StringBuilder(half);
+
+        if (middle != 0) {
+            result.append(middle);
+        }
+
+        for (int i = half.length() - 1; i >= 0; --i) {
+            result.append(half.charAt(i));
+        }
+
+        return result.toString();
+    }
+
+    private String smallestGreaterOrEqual(int[] originalCount, String targetHalf) {
+        int[] count = originalCount.clone();
+        int k = targetHalf.length();
+        int matched = 0;
+
+        while (matched < k && count[targetHalf.charAt(matched) - 'a'] > 0) {
+            count[targetHalf.charAt(matched) - 'a']--;
+            matched++;
+        }
+
+        if (matched == k) {
+            return targetHalf;
+        }
+
+        for (int pos = matched; pos >= 0; --pos) {
+            if (pos < matched) {
+                count[targetHalf.charAt(pos) - 'a']++;
+            }
+
+            for (int c = targetHalf.charAt(pos) - 'a' + 1; c < 26; ++c) {
+                if (count[c] == 0) continue;
+
+                StringBuilder result = new StringBuilder(targetHalf.substring(0, pos));
+                result.append((char) ('a' + c));
+                count[c]--;
+
+                for (int ch = 0; ch < 26; ++ch) {
+                    while (count[ch]-- > 0) {
+                        result.append((char) ('a' + ch));
+                    }
+                    count[ch] = Math.max(count[ch], 0);
+                }
+
+                return result.toString();
             }
         }
 
-        if(mid!='#'){
-            // odd-length palindrome: left half + mid + reverse(left half)
-            String temp = cur;
-            cur += mid;
-            temp = new StringBuilder(temp).reverse().toString();
-            cur += temp;
-        }
-        else {
-            // even-length palindrome: left half + reverse(left half)
-            String temp = cur;
-            temp = new StringBuilder(temp).reverse().toString();
-            cur += temp;
+        return "";
+    }
+
+    private boolean nextPermutation(char[] half) {
+        int pivot = half.length - 2;
+
+        while (pivot >= 0 && half[pivot] >= half[pivot + 1]) {
+            pivot--;
         }
 
-        // feasibility check: only valid if this (largest possible) candidate beats target
-        return cur.compareTo(target) > 0 ? cur : "";
+        if (pivot < 0) {
+            return false;
+        }
+
+        int swapPos = half.length - 1;
+
+        while (half[swapPos] <= half[pivot]) {
+            swapPos--;
+        }
+
+        char temp = half[pivot];
+        half[pivot] = half[swapPos];
+        half[swapPos] = temp;
+
+        int left = pivot + 1;
+        int right = half.length - 1;
+
+        while (left < right) {
+            temp = half[left];
+            half[left] = half[right];
+            half[right] = temp;
+            left++;
+            right--;
+        }
+
+        return true;
     }
 
     public String lexPalindromicPermutation(String s, String target) {
-        int n = s.length();
+        int[] frequency = new int[26];
 
-        int[] freq = new int[26];
-
-        if(n==1){
-            if(s.compareTo(target) > 0) return s;
-            else return "";
+        for (char ch : s.toCharArray()) {
+            frequency[ch - 'a']++;
         }
 
-        for(char c : s.toCharArray())
-            freq[c-'a']++;
-
-        char mid = '#';
+        char middle = 0;
         int oddCount = 0;
 
-        for(int i=0; i<26; i++){
-            if(freq[i]%2 != 0){
-                // odd count -> this becomes the middle character
-                mid = (char)('a'+i);
-                freq[i]--;
+        for (int c = 0; c < 26; ++c) {
+            if ((frequency[c] & 1) == 1) {
                 oddCount++;
+                middle = (char) ('a' + c);
             }
-
-            freq[i] /= 2; // each char used freq[i]/2 times in the left half
-
-            if(oddCount>=2) return ""; // more than one odd-frequency char -> can't form a palindrome
         }
 
-        n /= 2; // we only need to construct the left half now
-
-        String res = "", prefix = "";
-
-        // greedily build the left half, position by position
-        for(int i=0; i<n; i++){
-
-            String cur = prefix;
-            boolean isThereAny = false;
-
-            // try smallest character first ('a' -> 'z')
-            for(int j=0; j<26; j++){
-
-                if(freq[j] > 0){
-
-                    freq[j]--;
-                    cur += (char)('a'+j);
-
-                    // check if this prefix can still lead to a palindrome > target
-                    String isPos = isPossible(n, freq, cur, mid, target);
-
-                    if(!isPos.equals("")){
-                        prefix = cur;      // keep this character, lock in the prefix
-                        isThereAny = true;
-
-                        if(res.equals(""))
-                            res = isPos;
-                        else
-                            res = res.compareTo(isPos) < 0 ? res : isPos; // track smallest valid candidate
-
-                        break;
-                    }
-
-                    // this character doesn't work, undo and try the next one
-                    freq[j]++;
-                    cur = cur.substring(0, cur.length()-1);
-                }
-            }
-
-            if(!isThereAny)
-                return ""; // no character works at this position -> impossible
+        if (oddCount > 1) {
+            return "";
         }
 
-        return res; 
+        int[] halfCount = new int[26];
+
+        for (int c = 0; c < 26; ++c) {
+            halfCount[c] = frequency[c] / 2;
+        }
+
+        int k = s.length() / 2;
+        String targetHalf = target.substring(0, k);
+
+        String halfString = smallestGreaterOrEqual(halfCount, targetHalf);
+
+        if (halfString.isEmpty() && k > 0) {
+            return "";
+        }
+
+        String candidate = buildPalindrome(halfString, middle);
+
+        if (candidate.compareTo(target) > 0) {
+            return candidate;
+        }
+
+        char[] half = halfString.toCharArray();
+
+        if (!nextPermutation(half)) {
+            return "";
+        }
+
+        return buildPalindrome(new String(half), middle);
     }
 }
